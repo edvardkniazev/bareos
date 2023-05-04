f69170b0023e493b487953a4472c3e2970c6294a
edvardkniazev <edvard.knyazev@gmail.com>
2023-05-04 21:54:24 +0300

    Add functions get_url() and login()

---
+++ src/app.py
@@ -0,0 +1,72 @@
+import httpx
+import configparser
+from pprint import pprint   # only for debug purpose
+
+
+config_file="config.ini"
+
+
+def get_config_data(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    hostname = config.get("Common", "hostname")
+    port = config.get("Common", "port")
+    
+    config_data = {
+        "hostname": hostname,
+        "port": port
+        }
+
+    return config_data
+
+
+def get_auth_data(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    username = config.get("Authentication", "username")
+    password = config.get("Authentication", "password")
+    
+    auth_data = {
+        "username": username,
+        "password": password
+        }
+
+    return auth_data
+    
+
+def get_url(config_data):
+    hostname = config_data["hostname"]
+    port = config_data["port"]
+    url = f"http://{hostname}:{port}"
+    return url 
+
+
+def login(url, auth_data):
+    auth_response = httpx.post(f"{url}/token", data=auth_data)
+    access_token = auth_response.json()["access_token"]
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+        }
+    return headers
+
+
+if __name__ == "__main__":
+    config_data = get_config_data(config_file)
+    auth_data = get_auth_data(config_file)
+    url = get_url(config_data)
+    headers = login(url, auth_data)
+
+total_volumes = httpx.get(f"{url}/control/volumes", headers=headers)
+volumes = total_volumes.json()["volumes"]
+
+for pool in volumes.values():
+    for vol in pool:
+        #if vol["recycle"]:
+        if vol["volumename"] == "Wal-31420":
+            volume_id = vol["mediaid"]
+            volume_name = vol["volumename"]
+            #read_volume = httpx.delete(f"http://bareos.svc.ot.ru:8000/control/volumes/{volume_name}", headers=headers)
+            read_volume = httpx.get(f"http://bareos.svc.ot.ru:8000/control/volumes/{volume_id}", headers=headers)
+            pprint(str(read_volume.content))
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,11 @@
 from src.app import *
 
 
+config_file = "config.ini.example"
+
+
 def test_get_config_data():
-    config_data = get_config_data()
+    config_data = get_config_data(config_file)
     assert "hostname" in config_data.keys()
     assert "port" in config_data.keys()
     assert config_data["hostname"] is not None
@@ -10,8 +13,16 @@ def test_get_config_data():
 
 
 def test_get_auth_data():
-    auth_data = get_auth_data()
+    auth_data = get_auth_data(config_file)
     assert "username" in auth_data.keys()
     assert "password" in auth_data.keys()
     assert auth_data["username"] is not None
     assert auth_data["password"] is not None
+
+
+def test_get_url():
+    config_data = {
+        "hostname": "www.example.com",
+        "port": 80
+        }
+    assert get_url(config_data) == "http://www.example.com:80"
