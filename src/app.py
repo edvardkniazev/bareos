d4c22822982ebb835a3299171714c7bbd4d83998
edvardkniazev <edvard.knyazev@gmail.com>
2023-05-06 18:46:21 +0300

    Add logging

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 import httpx
 import configparser
+import os
+import logging
+from datetime import datetime
 from pprint import pprint   # only for debug purpose
 
 
@@ -10,12 +13,11 @@ def get_config_data(config_file):
     config = configparser.ConfigParser()
     config.read(config_file)
 
-    hostname = config.get("Common", "hostname")
-    port = config.get("Common", "port")
-    
     config_data = {
-        "hostname": hostname,
-        "port": port
+        "hostname": config.get("Common", "hostname"),
+        "port":     config.get("Common", "port"),
+        "storage":  config.get("Common", "storage"),
+        "logfile":  config.get("Common", "logfile")
         }
     return config_data
 
@@ -50,22 +52,47 @@ def login(url, auth_data):
     return headers
 
 
+def list_volumes_to_remove(url, headers):
+    response = httpx.get(url=url, headers=headers)
+    volumes = response.json()["volumes"]
+    volumes_to_remove = []
+
+    for pool in volumes.values():
+        v_to_d = [vol["volumename"] for vol in pool if vol["volstatus"] == 'Purged']
+        volumes_to_remove.extend(v_to_d)
+    return volumes_to_remove
+
+
+def make_requests_to_remove(url, volumes):
+    requests = [f"{url}/{v}" for v in volumes]
+    return requests
+
+
+def add_path_to_files(path, files):
+    files = [os.path.join(path, file) for file in files]
+    return files
+
+
+def remove_volumes(requests, headers, files):
+    for request, file in zip(requests, files):
+        #httpx.delete(url=request, headers=headers)
+        logging.debug(request)
+        #os.remove(file)
+        logging.debug(file)
+
+
 if __name__ == "__main__":
+
     config_data = get_config_data(config_file)
     auth_data = get_auth_data(config_file)
+
+    logging.basicConfig(filename=config_data["logfile"], level=logging.DEBUG)
+
     url = get_url(config_data)
     headers = login(url, auth_data)
 
-    total_volumes = httpx.get(f"{url}/control/volumes", headers=headers)
-    volumes = total_volumes.json()["volumes"]
-    
+    volumes = list_volumes_to_remove(url=f"{url}/control/volumes", headers=headers)
+    requests = make_requests_to_remove(url=f"{url}/control/volumes", volumes=volumes)
+    files = add_path_to_files(config_data["storage"], volumes)
 
-    for pool in volumes.values():
-        for vol in pool:
-            #if vol["recycle"]:
-            if vol["volumename"] == "Wal-31421":
-                volume_id = vol["mediaid"]
-                volume_name = vol["volumename"]
-    
-                read_volume = httpx.delete(f"{url}/control/volumes/{volume_name}", headers=headers)
-                pprint(str(read_volume.content))
+    remove_volumes(requests, headers, files)
