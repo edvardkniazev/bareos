baefa6cd58814721e3e630469a2821e2a01dc0f4
edvardkniazev <edvard.knyazev@gmail.com>
2023-05-12 20:23:20 +0300

    Migration to Classes

--- bin/app.py
+++ bin/app.py
@@ -8,95 +8,101 @@ from datetime import datetime
 config_file="etc/config.ini"
 
 
-def get_config_data(config_file):
-    config = configparser.ConfigParser()
-    config.read(config_file)
+class Config:
+    def __init__(self, config_file: str):
+        config = configparser.ConfigParser()
+        config.read(config_file)
 
-    return {
-        "hostname": config.get("Common", "hostname"),
-        "port":     config.get("Common", "port"),
-        "storage":  config.get("Common", "storage"),
-        "logfile":  config.get("Common", "logfile")
-        }
+        self.hostname = config.get("Common", "hostname")
+        self.port     = config.get("Common", "port")
+        self.storage  = config.get("Common", "storage")
+        self.logfile  = config.get("Common", "logfile")
+
+        self.username = config.get("Authentication", "username")
+        self.password = config.get("Authentication", "password")
 
 
-def get_auth_data(config_file):
-    config = configparser.ConfigParser()
-    config.read(config_file)
+class Url:
+    def __init__(self, cfg: Config):
+        self.base = f"http://{cfg.hostname}:{cfg.port}"
+        self.token = f"{self.base}/token"
+        self.control_volumes = f"{self.base}/control/volumes"
+        self.path = cfg.storage
 
-    username = config.get("Authentication", "username")
-    password = config.get("Authentication", "password")
+
+class Authorization():
+    def __init__(self, cfg: Config):
+        self.auth_data = {
+                "username": cfg.username,
+                "password": cfg.password
+                }
+
+    def login(self, url:str):
+        auth_response = httpx.post(f"{url}", data=self.auth_data)
+        access_token = auth_response.json()["access_token"]
+        self.headers = {
+            "Authorization": f"Bearer {access_token}"
+            }
+        return self.headers
+
+
+class Volumes():
+    def __init__(self, url: Url, headers: dict):
+        self.purged_volumes = []
+        self.volume_files = []
+        self.url = url
+        self.headers = headers
+
+    def find_purged_volumes(self):
+        response = httpx.get(url=self.url.control_volumes, headers=self.headers)
+        all_volumes = response.json()["volumes"]
     
-    return {
-        "username": username,
-        "password": password
-        }
-    
+        for pool in all_volumes.values():
+            pd = [vol["volumename"] for vol in pool if vol["volstatus"] == 'Purged']
+            self.purged_volumes.extend(pd)
+        self.volume_files = self.purged_volumes.copy()
 
-def get_url(config_data):
-    hostname = config_data["hostname"]
-    port = config_data["port"]
-    return f"http://{hostname}:{port}"
+    def set_requests(self):
+        return [f"{self.url.control_volumes}/{v}" for v in self.purged_volumes]
 
+    def set_paths(self):
+        return [os.path.join(self.url.path, file) for file in self.volume_files]
 
-def login(url, auth_data):
-    auth_response = httpx.post(f"{url}/token", data=auth_data)
-    access_token = auth_response.json()["access_token"]
-    return {
-        "Authorization": f"Bearer {access_token}"
-        }
+    def remove_volumes(self):
+        for request in self.set_requests():
+            #response = httpx.delete(url=request, headers=headers)
+            #if response.status_code == 204:
+            if True:
+                logging.info(f"Removed successfully {request}")
+            else:
+                logging.error(f"{response.status_code} {request}")
 
-
-def list_volumes_to_remove(url, headers):
-    response = httpx.get(url=url, headers=headers)
-    volumes = response.json()["volumes"]
-    volumes_to_remove = []
-
-    for pool in volumes.values():
-        v_to_d = [vol["volumename"] for vol in pool if vol["volstatus"] == 'Purged']
-        volumes_to_remove.extend(v_to_d)
-    return volumes_to_remove
-
-
-def remove_volumes(url, headers, volumes):
-    requests = [f"{url}/control/volumes/{v}" for v in volumes]
-    for request in requests:
-        #response = httpx.delete(url=request, headers=headers)
-        #if response.status_code == 204:
-        if True:
-            logging.info(f"Removed successfully {request}")
-        else:
-            logging.error(f"{response.status_code} {request}")
-
-
-def remove_files(path, files):
-    path_files = [os.path.join(path, file) for file in files]
-    for file in path_files:
-        try:
-            #os.remove(file)
-            logging.info(f"Removed successfully {file}")
-        except OSError as e:
-            logging.error(f"{e.strerror} {file}")
+    def remove_files(self):
+        for file in self.set_paths():
+            try:
+                #os.remove(file)
+                logging.info(f"Removed successfully {file}")
+            except OSError as e:
+                logging.error(f"{e.strerror} {file}")
 
 
 if __name__ == "__main__":
 
     start = datetime.now()
 
-    config_data = get_config_data(config_file)
-    auth_data = get_auth_data(config_file)
+    cfg = Config(config_file)
 
-    logging.basicConfig(filename=config_data["logfile"], level=logging.INFO)
+    logging.basicConfig(filename=cfg.logfile, level=logging.INFO)
     logging.info(f'Start time: {start.strftime("%Y-%m-%d %H:%M:%S")}')
 
-    url = get_url(config_data)
-    headers = login(url, auth_data)
+    url = Url(cfg)
+    auth = Authorization(cfg)
+    headers = auth.login(url.token)
+    volumes = Volumes(url, headers)
 
-    volumes = list_volumes_to_remove(f"{url}/control/volumes", headers)
-    remove_volumes(url, headers, volumes)
-
-    files = volumes.copy()
-    remove_files(config_data["storage"], files)
+    volumes.find_purged_volumes()
+    volumes.remove_volumes()
+    volumes.remove_files()
 
     end = datetime.now()
     elapsed = end - start
